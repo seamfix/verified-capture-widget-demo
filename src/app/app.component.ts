006915a74b6ca5c3aeae7ec67d3ecc3da94aa282
Sulaiman Olaosebikan <solaosebikan@seamfix.com>
2023-08-03 10:01:46 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cature-widget';
+  token = '2514df1a031570a45b1089ffdf89b11f3f42d7bd397a9598af4f143e33c5746ce21e9269c1d3e443621fa905ca5741655575d5dceb6937ce83625276b80bdee1671224c5333d715310704c3cf1c1c157'
+  authorisation = {
+    tokenId: 'VF-1681314419154',
+    authenticationToken: '9ccaa6727381d578dff2f6d651b8f8ec2a42176ce391c4c1572f20fc840fa9da7463235a3c424f7c91ff4eb704c37e5320427bc83a43bb89e2ae1d710556052c902127160cf5f93eae3fa8d9f38076af',
+    linkIdentifier: 'Sulaiman106',
+    callbackUrl: 'https://webhook.site/7e848f5d-5aae-4c41-a15b-032a88355a40',
+    personalizationDetail: 'Sulaiman',
+    baseUserId: 1630321373727
+  }
+  submit(e: any){
+    console.log("capture response", e)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { SfxCaptureModule } from '@seamfix/sfx-capture';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SfxCaptureModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
